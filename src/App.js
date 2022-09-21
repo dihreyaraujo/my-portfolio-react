ed159eb0f262394d77b68865fd124c71bbb22bcd
Dihrey Araujo <99283526+dihreyaraujo@users.noreply.github.com>
2022-09-21 19:19:12 -0300

    error building trying3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import HardSkills from './pages/HardSkills';
 import Certificados from './pages/Certificados';
@@ -10,12 +10,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Switch>
-          <Route exact path="/" component={ Home } />
-          <Route path="/hardskills" component={ HardSkills } />
-          <Route path="/certificados" component={ Certificados } />
-          <Route path="/projetos" component={ Projetos } />
-        </Switch>
+        <Routes>
+          <Route exact path="/" element={ <Home /> } />
+          <Route path="/hardskills" element={ <HardSkills /> } />
+          <Route path="/certificados" element={ <Certificados /> } />
+          <Route path="/projetos" element={ <Projetos /> } />
+        </Routes>
       </div>
     );
   }
